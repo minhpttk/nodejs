ea4a1b4c70d90d8c29288d2a54ca9a7c59dba505
minhpttk <dinhminh060102@gmail.com>
2025-04-06 17:18:29 +0700

    middleware

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const apikey = require('../services/apikey.service');
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+const apiKey = async (req, res, next) => {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString();
+        if (!key) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            })
+        }
+        //check objKey
+        const objKey = await apikey.findById(key);
+        if (!objKey) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            })
+        }
+        req.objKey = objKey;
+        return next();
+        
+    } catch (error) {
+        
+    }
+}
+
+const checkPermission = (permission) => {
+    return (req, res, next) => {
+        if (!req.objKey.permissions.includes(permission)) {
+            return res.status(403).json({message: 'Permission Denied'})
+        }
+        return next();
+    }
+}
+
+module.exports = {
+    apiKey,
+    checkPermission
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,12 @@
 
 const express = require('express')
 const router = express.Router()
+const {apiKey, checkPermission} = require('../auth/checkAuth');
+
+//check apikey
+router.use(apiKey);
+//check permission
+router.use(checkPermission('0000'));
 
 router.use('/api/v1', require('./access'))
 
